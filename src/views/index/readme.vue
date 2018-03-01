e9d7caef9818760150e98c85daeb7f192b25e9cf
HuangChongqing <1756260160@qq.com>
2018-03-02 02:52:31 +0800

    init

---
+++ src/views/index/readme.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="app-container">
+        <code>
+            <h3>系统说明：</h3>
+            <ul>
+                <li>此项目为本系统前端部分（vue2.0框架开发），数据接口将用nodeJs(或eggJs)开发，并用MongoDB数据库进行数据存储</li>
+                <li>目前所有数据（用户登录，获取表格表单信息等）都为本地json数据模拟，待后台数据接口完成，可把json地址切换成实际开发数据接口地址，</li>
+                <li>目前项目可访问<a href="https://github.com/HuangCongQing/vue-semanticNetwork">地址</a>更多功能正在开发中...</li>
+                <li>... ...</li>
+            </ul>
+        </code>
+
+  </div>
+</template>
+
+<script>
+//  import { mapGetters } from 'vuex';
+// import {global} from 'src/global/global';
+// import {api} from 'src/global/api';
+
+export default {
+  data() {
+    return {
+        
+    }
+  },
+  computed: {
+        // ...mapGetters({
+        //     personalInfo_init:'userInfo'
+        // })
+    },
+  mounted() {
+    
+   },
+  methods: {
+    
+
+  }
+};
+</script>
+
+<style rel="stylesheet/scss" lang="scss">
+    
+</style>
